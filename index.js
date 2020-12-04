09f9432f24e7e40bdd030572c764184987ee35f4
ปฏิภาณ สุวานิช <Patiphan.suwanich@krungsri.com>
2020-12-04 22:01:34 +0700

    add axios , test reply line

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express = require('express')
+const axios = require('axios');
 
 const app = express()
 
 const PORT = process.env.PORT || 8080
 
-app.get('/', (req, res) => { res.send('Hello World') });
+const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message';
+const LINE_HEADER = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer 7MxMoId+Cf9MJNaQr+YMexcez9Q/5+SLjHpCOmMhnDGI4nLKQGiz/Ch7cVSf6VYn+/Mgqc7UA4bysVs8qIFT+Qi5oWWZZpoi6p4Wr+CEx3c0575W+ksZ5ssBfZPMerTWl0LIRmtC/QyyzOFaGCbJLAdB04t89/1O/w1cDnyilFU=`
+};
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port : ${PORT}`)
+app.post('/lineBot', (req, res) => {
+    if (req.body.events[0].message.type !== 'text') {
+        return;
+    }
+    reply(req.body);
+    res.sendStatus(200)
 })
 
-module.exports = app
+const reply = (bodyResponse) => {
+    return axios({
+        method: 'post',
+        uri: `${LINE_MESSAGING_API}/reply`,
+        headers: LINE_HEADER,
+        body: JSON.stringify({
+            replyToken: bodyResponse.events[0].replyToken,
+            messages: [
+                {
+                    type: `text`,
+                    text: bodyResponse.events[0].message.text
+                }
+            ]
+        })
+    });
+};
+
+
+app.listen(PORT)
