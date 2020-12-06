b4219dfec6d2d5e2ef1ab0b7095995d9e909d0b9
ปฏิภาณ สุวานิช <Patiphan.suwanich@krungsri.com>
2020-12-06 22:26:46 +0700

    set to update

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ const addToCounting = async (data, event) => {
         } else {
             countingSnapshot.forEach((doc) => {
                 console.log(doc.id, ' => ', doc.data())
-                db.collection("counting").doc(doc.id).set({
+                db.collection("counting").doc(doc.id).update({
                     date: callDate(data.date),
                     step: data.step,
                 }).then(function () {
